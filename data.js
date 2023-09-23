89ca09002556ff0453e8a14177ecca8f52e2f33c
csysides <cecilialujansides85@hotmail.com>
2023-09-23 09:56:15 -0300

    js2

---
+++ data.js
@@ -0,0 +1,27 @@
+const productsData = [
+    {
+      id: 1,
+      name: "minimaceta",
+      price: 2500,
+      category: "Macetita",
+      cardImg: "./imagenes/MiniMaceta.jpg",
+    }, {
+        id: 2,
+        name: "macetas",
+        price: 3500,
+        category: "__macetas",
+        cardImg: "./imagenes/Macetas.jpg",
+      }, {
+        id: 3,
+        name: "portadifusor",
+        price: 3000,
+        category: "__portadifusor",
+        cardImg: "./imagenes/portadifuFlor.jpg",
+      }, {
+        id: 4,
+        name: "alfombras",
+        price: 5000,
+        category: "alfombra_piecitos",
+    cardImg: "./imagenes/alfombra.jpg",
+      } 
+      
